5726b0606f900360045d0cf7dda0dff83b2e567a
fanto <58596314+fantoSama@users.noreply.github.com>
2023-05-12 15:50:24 +0200

    add html files

---
+++ code/html/ajaxGithub.js
@@ -0,0 +1,23 @@
+﻿document.getElementById('button-hub').addEventListener('click', loadGitHubUsers);
+
+function loadGitHubUsers() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/users', true);
+    xhr.onload = function () {
+        let users = JSON.parse(this.responseText);
+        let output = '';
+        for (let i in users) {
+            output +=
+                '<div class="user">' +
+                '<img src="' + users[i].avatar_url + '" width="70" height="70">' +
+                '<ul>' +
+                '<li>ID: ' + users[i].id + '</li>' +
+                '<li>Name: ' + users[i].login + '</li>' +
+                '<li>Url: ' + users[i].url + '</li>' +
+                '</ul>' +
+                '</div>';
+        }
+        document.getElementById('users').innerHTML = output;
+    }
+    xhr.send();
+}
