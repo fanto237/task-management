7365b7e7352bf5e7e6c6cb0b1d9b0f92a4d0c536
fanto <58596314+fantoSama@users.noreply.github.com>
2023-05-14 09:34:09 +0200

    set up final app directory

--- app/config/database.php
+++ app/config/database.php
@@ -1,11 +1,10 @@
 <?php
 
-use function PHPSTORM_META\type;
 
 define('DB_HOST', 'database');
 define('DB_USER', 'root');
 define('DB_PASS', 'fanto237');
-define('DB_NAME', 'QuoteActionDb');
+define('DB_NAME', 'InfraDb');
 
 // Create database connection
 $conn = new \mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -15,11 +14,8 @@ if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 }
 
-// Connecting to Redis server 
+//connecting to redis database
+//Connecting to Redis server on localhost 
 $redis = new Redis();
 $redis->connect('redis');
 echo "Connection to server sucessfully";
-//check whether server is running or not 
-if ($redis->ping()) {
-    echo "PONG";
-}
