0aadfcfe6cf743522633dacd1cee953f7547f441
fanto <58596314+fantoSama@users.noreply.github.com>
2023-05-18 17:51:25 +0200

    add new database transactions

--- app/server.php
+++ app/server.php
@@ -1,60 +1,63 @@
 <?php
-
+session_start();
 include 'config/database.php';
 
 $username = $_POST['username'];
 $password = $_POST['password'];
-$status = 'offline';
 
 
 
 if (isset($_POST['status'])) {
-    echo 'status is set';
-    $status = $_POST['status'];
-    loginSessin($username, $password, $status, $conn);
+    loginSessin($username, $password, $conn);
 } else {
-    echo 'status is not set';
-    addNewUser($username, $password, $status, $conn);
+    addNewUser($username, $password, $conn);
 }
 
 // method used to register a new user
-function addNewUser($username, $password, $status, $conn)
+function addNewUser($username, $password, $conn)
 {
     // check if the username already exists
     $sql = "SELECT * FROM user WHERE name = '$username'";
     $result = mysqli_query($conn, $sql);
+
     if (mysqli_num_rows($result) > 0) {
-        echo 'username already exists';
-        return;
+        echo 'register.php?error=Benutzername existiert bereits';
+        exit();
     }
 
-    echo 'username not  exists';
     // encrypt password
     $pass = md5($password);
 
     // add a new use into the user table
-    $sql = "INSERT INTO user (name, password, status) VALUES ('$username', '$pass', '$status')";
+    $sql = "INSERT INTO user (name, password) VALUES ('$username', '$pass')";
     $result = mysqli_query($conn, $sql);
-
-    // TODO not forget to delete this part
-    if ($result)
-        echo 'It worked just fine';
-    else
-        echo 'the problem is' . mysqli_error($conn);
+    echo 'login.php';
 }
 
 // method use for handling a loggin session
-function loginSessin($username, $password, $status, $conn)
+function loginSessin($username, $password, $conn)
 {
     // check if the login data are correct
+    // validate form data
+    $name = test_input($username);
+    $pass = md5(test_input($password));
 
-    $pass = md5($password);
-    echo "the password is: " . $pass;
-    $sql = "SELECT * FROM user WHERE name = '$username' AND password = '$pass' AND status = '$status'";
+    $sql = "SELECT * FROM user WHERE name = '$name' AND password = '$pass'";
+
     $result = mysqli_query($conn, $sql);
-    // if ($result)
-    if (mysqli_num_rows($result) > 0)
-        echo 'the login data are correct';
-    else
-        echo 'the login data are not correct';
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['username'] = $row['name'];
+        $_SESSION['password'] = $row['password'];
+        echo "home.php";
+    }
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
