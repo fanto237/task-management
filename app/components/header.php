52868795a4a5f03525856b7114cecf3e2c64450c
fanto <58596314+fantoSama@users.noreply.github.com>
2023-05-25 20:46:18 +0200

    update header infos

--- app/components/header.php
+++ app/components/header.php
@@ -16,19 +16,24 @@
 <body>
     <nav class="navbar navbar-expand-sm navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="/index.php">TM</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="/index.php">
+                <img src="../img/logo.png" alt="logo" width="40" height="40" class="logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="index.php">Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
+                        <a class="nav-link" href="index.php">Login</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
+    </div>
+    <div class="container d-flex justify-content-center align-items-center" style="min-height: 90vh;">
--- code/inc/header.php
+++ code/inc/header.php
@@ -21,7 +21,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="/index.php">Home</a>
+                        <a class="nav-link" href="/index.php">dashboard</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/feedback.php">Feedback</a>
